08745b20fb3938954fe819ae2910919bc89d0c0e
royzxq <royzxq@gmail.com>
2015-09-15 22:54:27 +0000

    update a little

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.mongoUri = "mongodb://royzxq:leetcode@ds027489.mongolab.com:27489/leetcode"
+
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var request = require("request");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var url = "http://localhost:3000/items.json";
+  console.log(url);
+  request(url, function(err, resp, body){
+      if(!err){
+          res.json(body);
+      } else{
+          res.status(500).json({status:"fail"});
+      }
+  });
 });
 
 module.exports = router;

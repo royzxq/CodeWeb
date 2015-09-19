18a3dfda0e2ea32f67e12913d2572f4686e6fa4a
royzxq <royzxq@gmail.com>
2015-09-19 19:53:11 +0000

    add python script

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var config = require("./config");

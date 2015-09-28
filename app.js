3f01555de48152c0e1905320fe9a4cfe3b3d3a3b
Xinquan <royzxq@gmail.com>
2015-09-27 22:38:29 -0700

    add minified version

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ mongoose.connect(config.mongoUri);
 var app = express();
 passportConfig();
 
+app.set('production', process.env.NODE_ENV === 'production');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
